d41959599b13f8d9c7fa2f6176d890dfb29dac88
Jimmie3 <lcx3452@gmail.com>
2024-09-05 17:47:35 +0800

    cancle limit router onReceived called only butter router

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -162,7 +162,7 @@ contract Router is UUPSUpgradeable,PausableUpgradeable,ReentrancyGuardUpgradeabl
         temp.amount = _amount;
         assert(address(pool) != address(0));
         temp.bridgeId = (uint64(poolId) << 56) | ++nonce; 
-        if(msg.sender != butterRouter) revert ONLY_BUTTER();
+        // if(msg.sender != butterRouter) revert ONLY_BUTTER();
         if(!pool.isSupport(temp.param.chainPoolToken)) revert NOT_SUPPORT(temp.param.chainPoolToken);
         IERC20Upgradeable(temp.param.chainPoolToken).safeTransferFrom(msg.sender,address(pool),temp.amount);
         emit OnReceived(
